a9cb33262059340e2e31d052622c64d3aadb0853
xmapst <xmapst@gmail.com>
2022-05-06 19:01:59 +0800

    update npm version to 16.15.0

--- webui/vue.config.js
+++ webui/vue.config.js
@@ -1,14 +1,11 @@
 module.exports = {
+    transpileDependencies: [],
     devServer: {
         port: 4400,
         open: true,
-        overlay: {
-            warnings: false,
-            errors: true
-        },
         proxy: {
             '/api': {
-                target: 'http://192.168.93.194:9999/',
+                target: 'http://wslhost:9999/',
                 changeOrigin: true,
             }
         }
